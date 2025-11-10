699025a8bae100cbd16d1bf544da6bc6325a5dc7
Monika Sunkler <monika.sunkler@arcor.de>
2025-11-10 15:26:46 +0100

    Voraussetzungen Anwendungserstellung
    module-info.java erstellt
    pom.xml angepasst

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.moni.medianizer {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.swing;
+    requires java.sql;
+
+    opens com.moni.medianizer.app to javafx.graphics, javafx.fxml;
+    opens com.moni.medianizer.app.view to javafx.graphics, javafx.fxml;
+    opens com.moni.medianizer.app.controller to javafx.graphics, javafx.fxml;
+    opens com.moni.medianizer.app.model to javafx.base;
+}
